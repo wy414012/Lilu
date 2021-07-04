871ea6ef9ec0384cbfa8a502ca8c8021e4b7564f
open-yaming <wumingquan@open-yamingdeiMac-Pro.local>
2021-07-04 14:42:29 +0800

    update sdk

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 git submodule update --init --recursive
 
+git submodule foreach git checkout master
+
+git submodule foreach git pull
+
 xcodebuild -jobs 1 -configuration Debug
 xcodebuild -jobs 1 -configuration Release
