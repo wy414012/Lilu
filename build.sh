c3db1a353c8164ed7f97d715a7b794b76f2c217c
open-yaming <wumingquan@open-yamingdeiMac-Pro.local>
2021-07-04 13:53:47 +0800

    del sumodules

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+git clone https://github.com/acidanthera/MacKernelSDK.git
 
 xcodebuild -jobs 1 -configuration Debug
 xcodebuild -jobs 1 -configuration Release
